c175f296cf701585f4ca4e8285b8660c719fbff3
Julyana Gusmão <julyanagcneiva@gmail.com>
2026-03-06 12:31:20 -0300

    refactor: changes route name to consistent

--- libris/src/routes/app.book.$id.tsx
+++ libris/src/routes/app.book.$id.tsx
@@ -1,6 +1,6 @@
-import BookPage from "@/pages/book/page"
+import { BookDetailPage } from "@/pages/books/book-detail-page"
 import { createFileRoute } from "@tanstack/react-router"
 
 export const Route = createFileRoute("/app/book/$id")({
-  component: BookPage,
+  component: BookDetailPage,
 })
--- libris/src/routes/app.books.tsx
+++ libris/src/routes/app.books.tsx
@@ -1,6 +1,13 @@
+
+import { BooksPage } from "@/pages/books/books-page"
 import { createFileRoute } from "@tanstack/react-router"
-import { BooksPage } from "@/pages/books/page"
 
 export const Route = createFileRoute("/app/books")({
+  validateSearch: (search: Record<string, unknown>) => {
+    return {
+      q: typeof search.q === "string" ? search.q : "",
+    }
+  },
+
   component: BooksPage,
 })
